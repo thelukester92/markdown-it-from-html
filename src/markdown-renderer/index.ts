6d2543f6a86729f9e0cb16ebfd1f739fd6e29b35
Luke Godfrey <thelukester92@me.com>
2024-09-10 10:15:39 -0500

    fix index

--- src/markdown-renderer/index.ts
+++ src/markdown-renderer/index.ts
@@ -1,1 +1,4 @@
-export { MarkdownRenderer } from './markdown-renderer';
+export * from './markdown-renderer';
+export * from './markdown-renderer-env';
+export * from './types';
+export * as utils from './utils';
